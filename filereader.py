7439a4ca020c07a88ba80413de9e029f88cbf725
choi seon jong <choisun0454@naver.com>
2024-02-20 17:09:15 +0900

    fixed filerader

--- filereader.py
+++ filereader.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 def filereader_status():
     return jsonify({'status': 'Service is up'})
 
-
-@app.route('/filereader', methods=['POST'])
+# 'GET' 메소드를 허용하고, 'file' 파라미터를 통해 파일 이름을 받습니다.
+@app.route('/filereader', methods=['GET'])
 def filereader():
-
-    flag_file_path = os.path.join(app.root_path, 'myrealpc')
+    # 'file' 파라미터를 사용하여 경로를 구성합니다.
+    file_name = request.args.get('file', '')
+    file_path = os.path.join(app.root_path, file_name)
 
     try:
-        # 파일을 읽고 내용을 반환합니다.
-        with open(flag_file_path, 'r') as file:
-            flag_content = file.read().strip()
-        
-        # FLAG 내용을 JSON 형식으로 응답
-        return jsonify(flag=flag_content)
+        # 파일을 열고 내용을 읽습니다.
+        with open(file_path, 'r') as file:
+            content = file.read().strip()
+        # 파일 내용을 JSON 형식으로 응답합니다.
+        return jsonify(content=content)
     except Exception as e:
-        app.logger.error(f"Error reading  file: {e}")
-        return "Error processing request", 400
+        app.logger.error(f"Error reading file: {e}")
+        return jsonify(error=str(e)), 400
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=7000)
